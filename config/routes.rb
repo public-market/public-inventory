59bb3906c56eafc4c26a5e0650a5aa845ca826b1
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-04-11 17:54:50 +0300

    Change routes to remove redundant 'api' prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#show'
 
-  scope :api do
-    namespace :v1 do
-      # expose only read-only API
-      jsonapi_resources :items, except: %i[create update destroy]
-    end
+  namespace :v1 do
+    # expose only read-only API
+    jsonapi_resources :items, except: %i[create update destroy]
   end
 end
