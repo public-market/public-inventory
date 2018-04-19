1ef4a49f87b0b5218e481f662d41623d5915bd86
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-04-19 13:12:26 +0300

    Add API scope for prefixing routes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,7 @@
 class ApiController < ActionController::API
   include JSONAPI::ActsAsResourceController
+
+  def base_url
+    @base_url ||= [request.protocol, request.host_with_port, ENV['API_SCOPE']].compact.join
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'home#show'
+  scope path: ENV['API_SCOPE'] do
+    root to: 'home#show'
 
-  namespace :v1 do
-    # expose only read-only API
-    jsonapi_resources :items, only: %i[index show]
-    jsonapi_resources :categories, only: %i[index show] do
-      jsonapi_relationships only: %i[index show]
+    namespace :v1 do
+      # expose only read-only API
+      jsonapi_resources :items, only: %i[index show]
+      jsonapi_resources :categories, only: %i[index show] do
+        jsonapi_relationships only: %i[index show]
+      end
     end
   end
 end
